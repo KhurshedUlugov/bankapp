d19729708c501312b9b57b40db08898f93f28d2c
KhurshedUlugov <kh.ulugov@gmail.com>
2020-09-21 17:41:39 +0500

    test

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -5,27 +5,31 @@ type Money int64
 type Currency string
 
 const (
-	TJS Currency = "TJS"
-	RUB Currency = "RUB"
-	USD Currency = "USD"
+        TJS Currency = "TJS"
+        RUB Currency = "RUB"
+        USD Currency = "USD"
 )
 
 type PAN string
 
 type Card struct {
-	ID int
-	PAN PAN
-	Balance Money
-	MinBalance Money
-	Currency Currency
-	Color string
-	Name string
-	Active bool
+        ID int
+        PAN PAN
+        Balance Money
+        MinBalance Money
+        Currency Currency
+        Color string
+        Name string
+        Active bool
 }
 
+// Category категории
+type Category string
+
 type Payment struct{
-	ID int
-	Amount Money
+        ID int
+        Amount Money
+        Category Category
 }
 
 type PaymentSource struct{
